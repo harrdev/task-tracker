69fa9d37ce58870f3b1441bcb23a00d0f7cd6bd2
harrdev <jasonharrphx@live.com>
2022-02-27 19:15:40 -0700

    created task tracking app

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
+  return (
+    <header>
+        <h1>{title}</h1>
+    </header>
+  )
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+    color: "red",
+    backgroundColor: "black"
+}
+
+export default Header
