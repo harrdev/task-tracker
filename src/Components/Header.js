bc482dc43cff1258a8eeb4325e3f529a3911e133
harrdev <jasonharrphx@live.com>
2022-02-27 20:19:15 -0700

    delete and rendering working

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,18 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = (e) => {
+        console.log("Clicked")
+    }
+    
   return (
-    <header>
+    <header className="header">
         <h1>{title}</h1>
+        <Button 
+        color="green" 
+        text="Add"
+        onClick={onClick} />
     </header>
   )
 }
@@ -12,9 +21,10 @@ Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
-const headingStyle = {
-    color: "red",
-    backgroundColor: "black"
-}
+// CSS in JS
+// const headingStyle = {
+//     color: "red",
+//     backgroundColor: "black"
+// }
 
 export default Header
