da7857d53c560550da6321302274bfc11a4d177e
harrdev <jasonharrphx@live.com>
2022-02-27 23:07:27 -0700

    added new task functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./Components/Header";
 import Tasks from "./Components/Tasks";
 import { useState } from "react";
+import Addtask from "./Components/Addtask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -18,18 +19,25 @@ function App() {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  }
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
   // Toggle Reminder
   const toggleReminder = (id) => {
-    console.log("ID is: ", id);
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder:!task.reminder } : task))
   };
 
   return (
     <div className="container">
       <Header title="Task Tracker" />
+      <Addtask addTask={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
